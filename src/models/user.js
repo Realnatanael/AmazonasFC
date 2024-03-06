3dbd6afee04dbff3dd56527156497ad6b35276b0
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-03-06 02:17:09 -0400

    aproveitei para criptografar a password com bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -8,15 +9,18 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        lowercase: true,
     },
     password: {
         type: String,
         required: true,
+        select: false,
     },
     avatar: {
         type: String,
@@ -27,6 +31,11 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
 });
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 const User = mongoose.model("User", UserSchema);
 
