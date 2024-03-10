a78b9325fb5fe1f3ebc442485c13380e29e9ae83
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-03-10 03:28:36 -0400

    Add a JWT in my ID's

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,8 @@
 import User from '../models/user.js'
+import jwt from 'jsonwebtoken'
 
 const loginService = (email) =>  User.findOne({email: email}).select("+password");
 
-export {loginService};
+const generateToken = (id) => jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400});
+
+export {loginService, generateToken};
