826ae31e24ec969d3acdc0fc4416003844964bce
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-03-13 22:05:48 -0400

    starting the protection JWT's path

--- src/models/news.js
+++ src/models/news.js
@@ -20,7 +20,7 @@ const NewsSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        require: true,
+        required: true,
     },
     likes:{
         type: Array,
