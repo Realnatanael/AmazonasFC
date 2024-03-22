013de684b2db0865351c12f1f65e936767d96d92
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-03-22 02:03:54 -0400

    API's data pagination

--- src/services/news.services.js
+++ src/services/news.services.js
@@ -2,9 +2,12 @@ import News from '../models/news.js'
 
 const createService = (body) => News.create(body);
 
-const findAllService = () => News.find();
+const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
+
+const countNews = () => News.countDocuments();
 
 export {
     createService, 
-    findAllService
+    findAllService,
+    countNews,
 }
