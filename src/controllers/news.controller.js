52400823dbdb0db167464fce748d2d4967382e68
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-03-24 23:46:20 -0400

    find the posts by id

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,6 +1,6 @@
-import {createService, findAllService, countNews, topNewsService} from "../services/news.services.js"
+import {createService, findAllService, countNews, topNewsService, findByIdService} from "../services/news.services.js"
 
-const create = async (req, res) => {
+export const create = async (req, res) => {
     try {
         const {title, text, banner} = req.body;
 
@@ -23,7 +23,7 @@ const create = async (req, res) => {
     }
 };
 
-const findAll = async (req, res) => {
+export const findAll = async (req, res) => {
     try {
     let {limit, offset} = req.query;
 
@@ -76,7 +76,7 @@ const findAll = async (req, res) => {
     }
 };
 
-const topNews = async (req, res) => {
+export const topNews = async (req, res) => {
     try{
     const news = await topNewsService();
 
@@ -101,4 +101,26 @@ const topNews = async (req, res) => {
     }
 }
 
-export {create, findAll, topNews}
+export const findById = async(req, res) => {
+    try{
+        const {id} = req.params;
+
+        const news = await findByIdService(id);
+
+        return res.send({
+            news: {
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                banner: news.banner,
+                likes: news.likes,
+                comments: news.comments,
+                name: news.user.name,
+                username: news.user.username,
+                userAvatar: news.user.avatar, 
+               },
+        });
+    } catch (err){
+        res.status(500).send({message: err.message});
+    }
+}
