c4eb70f298bc21e4dd13064221c33ee69d1ab792
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-03-26 00:45:17 -0400

    search by title and News/posts by user

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import {createService, findAllService, countNews, topNewsService, findByIdService} from "../services/news.services.js"
+import {createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService, byUserService} from "../services/news.services.js"
 
 export const create = async (req, res) => {
     try {
@@ -119,6 +119,57 @@ export const findById = async(req, res) => {
                 username: news.user.username,
                 userAvatar: news.user.avatar, 
                },
+        });
+    } catch (err){
+        res.status(500).send({message: err.message});
+    }
+}
+
+export const searchByTitle = async (req, res) => {
+    try{
+        const {title} = req.query;
+
+        const news =await searchByTitleService(title);
+
+        if(news.length === 0){
+            return res.status(400).send({message: "There are no posts with this title"});
+        }
+
+        return res.send({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar, 
+               })),
+        });
+    } catch (err){
+        res.status(500).send({message: err.message});
+    }
+}
+
+export const byUser = async (req,res) => {
+    try{
+        const id = req.userId;
+        const news = await byUserService(id);
+
+        return res.send({
+            results: news.map((item) => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                comments: item.comments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar, 
+               })),
         });
     } catch (err){
         res.status(500).send({message: err.message});
