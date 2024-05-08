172079085b063e9ca8ad99e1f4c7df4c2c75876f
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-08 04:11:00 -0400

    feat: Add lazy loading for images in news route

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -19,6 +19,9 @@ import { authMiddleware } from "../middlewares/auth.middlewares.js";
 router.post("/create", authMiddleware, create)
 router.get("/", findAll)
 router.get("/top", topNews)
+router.get("/", (req, res) => {
+    res.send("Hello, world!");
+  });
 router.get("/search", searchByTitle)
 router.get("/byUser", authMiddleware, byUser)
 router.get("/:id", authMiddleware, findById)
