ebae5916cd04430827b3c5baf49251ec781fc941
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-08 03:17:37 -0400

    feat: Update Node.js version requirement to 20.x
    The Node.js version requirement in package.json and package-lock.json was updated to 20.x to ensure compatibility with the specified Node.js version.
    
    Co-authored-by: Natanael

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import dotenv from "dotenv";
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
 import newsRoute from './src/routes/news.route.js';
-
 dotenv.config(); 
 
 const port = process.env.PORT || 3000; 
