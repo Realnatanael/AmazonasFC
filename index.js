cb6698e30debd0510ec23a7ca5070d0c5d80e585
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-08 04:45:01 -0400

    feat: Add CORS configuration for localhost:5173

--- index.js
+++ index.js
@@ -12,18 +12,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 connectDatabase();
-
-// Adicione este bloco de código logo após a inicialização do aplicativo express
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
-
+app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
