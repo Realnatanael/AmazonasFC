0e3897a115836139aff384e6b9b9b877222eeb1b
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-08 15:15:56 -0400

    feat: Add CORS headers for all routes
    This commit adds CORS headers to the server configuration, allowing requests from any origin. This ensures proper handling of CORS requests and improves the compatibility of the backend server with different frontend applications.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ const app = express();
 
 connectDatabase();
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello, world!");
