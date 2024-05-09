4f4dee28956b66228df2639c40a01ea43185127e
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-08 22:02:03 -0400

    feat: Update CORS headers to allow requests from specific origin
    This commit updates the CORS headers in both the server configuration and the Vercel deployment configuration. It changes the allowed origin from "*" to "https://amazonas-fc.vercel.app", ensuring that requests are only allowed from this specific origin. This improves security by restricting cross-origin requests to a trusted source.
    
    Co-authored-by: Natanael

--- src/index.js
+++ src/index.js
@@ -14,11 +14,6 @@ const app = express();
 connectDatabase();
 app.use(cors());
 app.options('*', cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello, world!");
