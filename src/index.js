0d82f1d9cfa6a0b125ea085ac6d48710629fff8f
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-08 21:21:15 -0400

    feat: Update CORS headers to allow additional methods and credentials

--- src/index.js
+++ src/index.js
@@ -15,9 +15,11 @@ connectDatabase();
 app.use(cors());
 app.options('*', cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-CSRF-Token, X-Api-Version, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
 });
 app.use(express.json());
 app.get("/", (req, res) => {
